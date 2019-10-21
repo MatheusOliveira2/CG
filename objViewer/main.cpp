b7389367d08d833426724f3a23aea60a68f07275
root <root@DESKTOP-NSMVKSC.localdomain>
2019-10-21 07:15:14 -0300

    Roda gigante

---
+++ objViewer/main.cpp
@@ -0,0 +1,162 @@
+// main.cpp - Wavefront OBJ viewer application
+
+// V-ART example: Load and Display a OBJ file.
+
+// This application shows how to create an object from a Wavefront OBJ file.
+
+// Made from picker example, keeps the piking code.
+
+// Changelog
+// Oct 19, 2012 - Bruno de Oliveira Schneider
+// - Adapted to new keyboard handler methods.
+// Sep 26, 2012 - Bruno de Oliveira Schneider
+// - Application created.
+
+#include <vart/scene.h>
+#include <vart/box.h>
+#include <vart/cylinder.h>
+#include <vart/light.h>
+#include <vart/meshobject.h>
+#include <vart/sphere.h>
+#include <vart/contrib/viewerglutogl.h>
+#include <vart/contrib/mousecontrol.h>
+
+#include <iostream>
+
+using namespace std;
+#define PI 3.14159265
+// Define the keyboard handler
+class KeyboardHandler : public VART::ViewerGlutOGL::KbHandler
+{
+    public:
+        KeyboardHandler() {
+        }
+        virtual void OnKeyDown(int key) {
+            switch (key)
+            {
+                case 'j':
+                    break;
+                case 'l':
+                    break;
+                case 'i':
+                    break;
+                case 'k':
+                    break;
+            }
+        }
+    private:
+};
+
+class ClickHandlerClass : public VART::MouseControl::ClickHandler
+{
+    public:
+        ClickHandlerClass() {
+        }
+        virtual ~ClickHandlerClass() {};
+        virtual void OnClick() {
+            if (mouseCtrlPtr->LastClickIsDown()) {
+                VART::MouseControl::ButtonID lastButton = mouseCtrlPtr->GetLastClickButton();
+
+                if (lastButton == VART::MouseControl::LEFT) {
+                    scenePtr->Pick(mouseCtrlPtr->GetClickXPosition(),
+                                   mouseCtrlPtr->GetClickYPosition(), &pickList);
+                    if (!pickList.empty()) {
+                        cout << pickList.size() << " selected objects: ";
+                        while (!pickList.empty()) {
+                            cout << pickList.front()->GetDescription() << ":\n";
+#ifdef DEBUG
+                            VART::MeshObject* ptObj = dynamic_cast<VART::MeshObject*>(pickList.front());
+                            if (ptObj)
+                                cout << *ptObj << "\n";
+#endif
+                            pickList.pop_front();
+                        }
+                        cout << endl;
+                    }
+                }
+            }
+        }
+        list<VART::GraphicObj*> pickList;
+        VART::Scene* scenePtr;
+};
+
+// The application itself:
+int main(int argc, char* argv[])
+{
+    VART::ViewerGlutOGL::Init(&argc, argv); // Initialize GLUT
+
+    if (argc < 2) {
+        cerr << "Fatal error: No filename given.\nUsage: " << argv[0] << " filename\n"
+             << "Where 'filename' is a Wavefront OBJ file.\n";
+        return 1;
+    }
+
+    static VART::Scene scene; // create a scene
+    static VART::ViewerGlutOGL viewer; // create a viewer (application window)
+
+    KeyboardHandler kbh; // create a keyboard handler
+    // create a camera (scene observer)
+    VART::Camera camera(VART::Point4D(0,0,6),VART::Point4D(0,0,0),VART::Point4D(0,1,0,0));
+    // create some objects
+    list<VART::MeshObject*> objects;
+    ClickHandlerClass clickHandler;
+
+    clickHandler.scenePtr = &scene;
+
+    // Initialize scene objects
+    VART::MeshObject::ReadFromOBJ(argv[1], &objects);
+
+    VART::MeshObject* chair = NULL;
+    VART::MeshObject* wheel = NULL;
+    VART::MeshObject* support = NULL;
+
+    // Build up the scene
+    //scene.AddObject(*iter);
+    VART::MeshObject* chairs[10];
+    list<VART::MeshObject*>::iterator iter = objects.begin();
+    for (; iter != objects.end(); ++iter) {
+        cout << "descrição: " << (*iter)->GetDescription()<< endl;
+
+        if((*iter)->GetDescription()=="chair"){
+            chair =*iter;
+            VART::Transform finishChairTransform;
+            double rad = PI/180;
+            for(int i = 1; i<=10;i++){
+                chairs[i]= dynamic_cast<VART::MeshObject*>(chair->Copy());
+                double z = cos(i*36*rad)*70;
+                double y = sin(i*36*rad)*70;
+                finishChairTransform.MakeTranslation(0,y,z);
+                chairs[i]->ApplyTransform(finishChairTransform);
+                scene.AddObject(chairs[i]);
+            }
+            
+        }
+
+        if((*iter)->GetDescription()=="wheel"){
+            wheel =*iter;
+            scene.AddObject(wheel);
+        }
+
+        if((*iter)->GetDescription()=="support"){
+            support =*iter;
+            scene.AddObject(support);
+        }
+
+    }
+
+    scene.AddLight(VART::Light::BRIGHT_AMBIENT());
+    scene.AddCamera(&camera);
+    scene.MakeCameraViewAll();
+
+
+    // Set up the viewer
+    viewer.SetTitle("V-ART OBJ Viewer");
+    viewer.SetScene(scene); // attach the scene
+    viewer.SetKbHandler(&kbh); // attach the keyboard handler
+    viewer.SetClickHandler(&clickHandler);
+
+    // Run application
+    scene.DrawLightsOGL(); // Set OpenGL's lights' state
+    VART::ViewerGlutOGL::MainLoop(); // Enter main loop (event loop)
+    return 0;
+}
